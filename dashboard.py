9c56e886b2d195aa020e6356645daeda4f4465a4
faridha96 <sih5@pge.com>
2026-08-06 08:59:36 -0700

    UPDATE Failure Type

--- dashboard.py
+++ dashboard.py
@@ -154,7 +154,7 @@ working_df[failure_type_col] = (
     .astype(str)
     .str.strip()
     .str.upper()
-    .str.replace("IN-SERVIEC ", "")
+    .str.replace("IN-SERVICE ", "")
 )
 
 working_df[failure_type_col] = (
